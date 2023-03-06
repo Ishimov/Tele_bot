6f46c84ddb91508548685137622217b7a273e76b
Ishimov <Tinkitop@yandex.ru>
2023-03-06 08:56:28 +0500

    Изменение названий функций (тест коммит)

--- main.py
+++ main.py
@@ -14,20 +14,17 @@ dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=['start'])
-async def send_welcome(msg: types.Message):
+async def cmd_start (msg: types.Message):
    await msg.answer(f'Я бот Jarvis. Приятно познакомиться, {msg.from_user.first_name}')
 
 @dp.message_handler(commands=['help'])
-async def send_help(msg: types.Message):
+async def cmd_help(msg: types.Message):
    await msg.answer('Скоро добавлю команды...')
 
 
 @dp.message_handler(content_types=['text'])
-async def get_text_messages(msg: types.Message):
-   if msg.text.lower() == 'привет':
-      await msg.answer('Привет!')
-   else:
-      await msg.answer('Не знаю такой команды.')
+async def get_text_messages(msg: types.Message) -> None:
+   await msg.answer(len(msg.text) - msg.text.count(' '))
 
 
 if __name__ == '__main__':
